5edbc5ddbb1a4be5ae6eba69095bcf2799f54d62
txkidxki <rattanakorn_re@cmu.ac.th>
2021-09-11 21:36:18 +0700

    change name mood

--- src/model/controller/GenItemList.java
+++ src/model/controller/GenItemList.java
@@ -18,14 +18,14 @@ public class GenItemList {
 //-------1.31
         public static ArrayList<BasedEquipment> setUpItemList() {
             ArrayList<BasedEquipment> itemLists = new ArrayList<BasedEquipment>(5);
-            itemLists.add(new Weapon("Sword",10, DamageType.magical,"assets/Smile.png"));
-            itemLists.add(new Weapon("Gun",20,DamageType.magical,"assets/Boring.png"));
-            itemLists.add(new Weapon("Staff",30,DamageType.magical,"assets/Panic.png"));
+            itemLists.add(new Weapon("HAPPY",10, DamageType.magical,"assets/Smile.png"));
+            itemLists.add(new Weapon("BORING",20,DamageType.magical,"assets/Boring.png"));
+            itemLists.add(new Weapon("PANIC",30,DamageType.magical,"assets/Panic.png"));
             //++Bow++
-            itemLists.add(new Weapon("Bow", 100,  DamageType.magical, "assets/Love.png"));
+            itemLists.add(new Weapon("LOVE", 100,  DamageType.magical, "assets/Love.png"));
             //=======
-            itemLists.add(new Armor("shirt",0,50,"assets/Sad.png"));
-            itemLists.add(new Armor("armor",50,0,"assets/Angry.png"));
+            itemLists.add(new Weapon("SAD",0,DamageType.magical,"assets/Sad.png"));
+            itemLists.add(new Weapon("ANGRY",50,DamageType.magical,"assets/Angry.png"));
             //++defense++
             itemLists.add(new Armor("defense", 98, 0, "assets/defense1.png"));
             //===========
